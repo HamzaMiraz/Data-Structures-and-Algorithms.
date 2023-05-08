8b2c76c8616b4a5132e33e3d61d920ff315ecb5e
HamzaMiraz <98533446+HamzaMiraz@users.noreply.github.com>
2023-05-08 16:18:16 +0600

    Add files via upload

--- Algorithms/DFS.cpp
+++ Algorithms/DFS.cpp
@@ -51,12 +51,15 @@ int par[V_SZ];
 
 int startTime[V_SZ];
 int finishTime[V_SZ];
+int Flattening_tree[2*V_SZ];
+int depth[V_SZ];
+int height[V_SZ];
+int subtree_sum[V_SZ];
 
 vector<int>order;
 
 int Time;
 int V,E;
-
 void init(){
     for(int i =1;i<=V;i++)
     {
@@ -65,6 +68,11 @@ void init(){
         g[i].clear();
 
         startTime[i] = finishTime[i] = -1;
+        Flattening_tree[i]=Flattening_tree[V-i+1]=-1;
+
+        height[i]=0;
+        depth[i]=0;
+        subtree_sum[i]=i;
     }
     Time = 1;
     order.clear();
@@ -72,14 +80,20 @@ void init(){
 
 void dfs(int u)
 {
-    startTime[u] = Time++;
+    startTime[u] = Time;
+    Flattening_tree[Time]=u;
+    Time++;
     col[u] = GRAY;
 
     for(auto v: g[u])
     {
-        if(col[v]==WHITE){
+        if(col[v]==WHITE){/// if we solve tree problem then we can use if(v!=par[u])condition in this line
+                            ///becouse tree do not contain cycle
+            depth[v]=depth[u]+1;
             par[v] = u;
             dfs(v);
+            height[u]=max(height[u],height[v]+1);
+            subtree_sum[u]+=subtree_sum[v];
         }
     }
 
@@ -87,7 +101,9 @@ void dfs(int u)
 
     order.push_back(u);
 
-    finishTime[u] = Time++;
+    finishTime[u] = Time;
+    Flattening_tree[Time]=u;
+    Time++;
 }
 
 
@@ -95,7 +111,7 @@ int main()
 {
 //    freopen("dfs.in","r",stdin);
 //    freopen("out.txt","w",stdout);
-
+    /// in tree problem E = V-1
     while(cin>>V>>E)
     {
         init();
@@ -117,11 +133,27 @@ int main()
         puts("Parent:");
         for(int i=1;i<=V;i++)   printf("[%d:%d], ",i, par[i]);  printf("\n");
 
-        puts("Time:");
+        puts("\nTime:");
         for(int i=1;i<=V;i++)   printf("%d:[%d-%d], ",i, startTime[i], finishTime[i]); puts("");
+        puts("\nFlattening Tree :");
+        for(int i=1;i<=2*V;i++)   printf("%d, ",Flattening_tree[i]); puts("");
 
-        puts("Final visited Order:");
+        puts("\ndepth of all vertex:");
+        for(int i=1;i<=V;i++){
+            cout<<i<<" -> "<<depth[i]<<" ,";
+        }
+        puts("\n\nheight of all vertex:");
+        for(int i=1;i<=V;i++){
+            cout<<i<<" -> "<<height[i]<<" ,";
+        }
+         puts("\n\nSub tree sum:");
+        for(int i=1;i<=V;i++){
+            cout<<"["<<i<<":"<<subtree_sum[i]<<"] ,";
+        }
+
+        puts("\n\nFinal complite  visited Order:");
         for(auto v: order)  printf("%d, ", v);  puts("");
+
     }
 
 
@@ -139,5 +171,31 @@ int main()
 3 4
 5 6
 5 7
+
+9 8
+
+1 2
+1 7
+2 3
+2 5
+3 4
+5 6
+7 8
+7 9
+
+13 12
+1 2
+1 3
+1 13
+2 5
+5 6
+5 7
+5 8
+8 12
+3 4
+4 9
+4 10
+10 11
+
 */
 
