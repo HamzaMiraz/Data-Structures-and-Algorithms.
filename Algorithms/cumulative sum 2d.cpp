4a42a5d0b2266d5d7eb61059032843c8a3908442
HamzaMiraz <98533446+HamzaMiraz@users.noreply.github.com>
2023-08-20 23:35:13 +0600

    Add files via upload

--- Algorithms/cumulative sum 2d.cpp
+++ Algorithms/cumulative sum 2d.cpp
@@ -46,8 +46,6 @@ struct debugger
     }
 } dbg;
 
-
-
 #define deb(a)     cout<<__LINE__<<"# "<<#a<<" -> "<<a<<endl;
 #define GCD(a,b)  __gcd(a,b)
 #define LCM(a,b)  (a*b)/__gcd(a,b)
@@ -60,36 +58,46 @@ int main()
 {
 //    freopen("in.txt", "r", stdin);  ///To read from a file.
 //    freopen("out.txt", "w", stdout);  ///To write  a file.
-int ar[100][100],r,c,px[100][100];
+///     one based;
+    int r,c;
     while(scanf("%d%d",&r,&c) ==2)
     {
-        for(int i=0;i<r;i++)
-            for(int j=0;j<c;j++)
+        int ar[r+5][c+5],px[r+5][c+5];
+        for(int i=1;i<=r;i++)
+            for(int j=1;j<=c;j++)
                 cin>>ar[i][j];
-        px[0][0]=ar[0][0];
-        for(int i=1;i<r;i++)px[i][0]=px[i-1][0]+ar[i][0];
-        for(int j=1;j<c;j++)px[0][j]=px[0][j-1]+ar[0][j];
+        
+        for(int i=0;i<=r;i++)px[i][0]=0;
+        for(int j=0;j<=c;j++)px[0][j]=0;
+        
+        px[1][1]=ar[1][1];
+        for(int i=2;i<=r;i++){
+            px[i][1]=px[i-1][1]+ar[i][1];
+            
+        }
+        for(int j=2;j<=c;j++){
+            px[1][j]=px[1][j-1]+ar[1][j];
+        }
 
-        for(int i=1;i<r;i++)
-            for(int j=1;j<c;j++){
+        for(int i=2;i<=r;i++)
+            for(int j=2;j<=c;j++){
                 px[i][j]=px[i-1][j]+px[i][j-1]+ar[i][j]-px[i-1][j-1];
             }
             cout<<"\nprefix sum array :"<<endl;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
+        for(int i=1;i<=r;i++){
+            for(int j=1;j<=c;j++){
                 cout<<px[i][j]<<" ";
             }
-            cout<<endl;
+            cout<<"\n";
         }
 
         cout<<"Range of summation array (i1,j1)->(i2,j2)"<<endl;
         int i1,i2,j1,j2,ans;
         cin>>i1>>j1>>i2>>j2;
+        if(i1>i2)swap(i1,i2);
+        if(j1>j2)swap(j1,j2);
 
-        if(i1==0)ans=px[i2][j2]-px[i2][j1-1];
-        else if(j1==0)ans=px[i2][j2]-px[i1-1][j2];
-        else if(i1==0&&j1==0)ans=px[i2][j2];
-        else ans=px[i2][j2]-px[i2][j1-1]-px[i1-1][j2]+px[i1-1][j1-1];
+        ans=px[i2][j2]-px[i2][j1-1]-px[i1-1][j2]+px[i1-1][j1-1];
 
         cout<<"sum of range = "<<ans<<endl;
 
