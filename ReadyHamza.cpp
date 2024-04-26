9edd713f511cf09444cddbd1bd4156fa56285fb5
HamzaMiraz <98533446+HamzaMiraz@users.noreply.github.com>
2024-04-26 11:50:32 +0600

    Add files via upload

--- ReadyHamza.cpp
+++ ReadyHamza.cpp
@@ -16,7 +16,7 @@ struct debugger
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 template<class T> using iset=tree<T,null_type,less<T>,
-rb_tree_tag,tree_order_statistics_node_update> ;///exmp= iset<double>st 
+rb_tree_tag,tree_order_statistics_node_update> ;///exmp= iset<double>st
 ///who is k'th position= *(set.find_by_order(k)),index of v = set.order_of_key(v);
 
 typedef long long ll;/// 1e18;
@@ -30,10 +30,6 @@ template <typename T>
 T INV(T base,T mod=1e9+7){//defult mod=1e9+7
     return BIGMOD(base%mod,mod-2,mod)%mod;///base^-1
 }
-
-template <typename T>
-vector<T>primeFactors(T n){vector<T>PRIME;T c=2;while///prime factor
-(n>1){if(n%c==0){PRIME.push_back(c);n/=c;}else c++;}return PRIME;}
 
 template <typename T>
 T nCr(T n, T r){T res=1;for (T i=0;i<r;i++){
@@ -55,8 +51,8 @@ reverse(dg.begin(),dg.end());return dg;}
 #define all(x) x.begin(), x.end()
 #define lbv(vec,x) lower_bound(all(vec),x)-vec.begin()//retrun index
 #define ubv(vec,x) upper_bound(all(vec),x)-vec.begin()
-#define lba(ar,n,x) lower_bound(ar,ar+n,x)-&ar[0];//return index        
-#define uba(ar,n,x) upper_bound(ar,ar+n,x)-&ar[0]; 
+#define lba(ar,n,x) lower_bound(ar,ar+n,x)-&ar[0];//return index
+#define uba(ar,n,x) upper_bound(ar,ar+n,x)-&ar[0];
 #define lb(mp,x) mp.lower_bound(x);///for set,map
 #define ub(st,x) st.upper_bound(x);///return pointer address
 #define GCD(a,b)  __gcd(a,b)
@@ -78,19 +74,27 @@ using minHeap = priority_queue<T, vector<T>, greater<T>>;
 
 const double PI = acos(-1);
 const double EPS = 1e-7; ///1*10^-7
-const int oo = 2e9+10;
-const ll MOD = 1e9+7;// Prime
+const int oo = 1e9+10;
+const ll MOD = 1e9 +7;// Prime
+
+int ks;
+void solve(){
+    //cout<<"Case "<<++ks<<": ";
+}
 
 int main()
 {
-//   freopen("in.txt", "r", stdin);  ///To read from a file.
-//    freopen("out.txt", "w", stdout);  ///To write  a file.
-//    ios_base::sync_with_stdio(0); cin.tie(0);
-    while(scanf("") ==)
-    {
-
+#ifndef ONLINE_JUDGE
+     freopen("inputf.in", "r", stdin);  ///To read from a file.
+//    freopen("outputf.in", "w", stdout);  ///To write  a file.
+#endif
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--){
+        solve();
     }
-    //cerr<<"\nTime:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n"; 
+    //cerr<<"\nTime:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
     return 0;
 }
 ///invisible =  
