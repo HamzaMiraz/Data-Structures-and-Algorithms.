8c94ac14a827dfbc6e4af99527692b822e8b302c
HamzaMiraz <98533446+HamzaMiraz@users.noreply.github.com>
2025-01-21 15:33:19 +0600

    Add files via upload

--- String algorithm/KMP.cpp
+++ String algorithm/KMP.cpp
@@ -6,32 +6,32 @@ vector<int>make_lps(string &s){
     for(int i=1;i<s.size();i++){
         int j=lps[i-1];
         while(j>0 && s[i]!=s[j])j=lps[j-1];
-        
+
         if(s[i]==s[j])lps[i]=++j;
     }
     return lps;
 }
 void kmp(string &txt,string &pat){
-    
+
     vector<int>lps=make_lps(pat);
-    
+
     int t(0),p(0);
     while(t<txt.size()){
         if(txt[t]==pat[p])++t,++p;
         else{
             if(p!=0)p=lps[p-1];
             else{
-                ++t;    
+                ++t;
             }
         }
         if(p==pat.size()){
             int pos=t-pat.size()+1;///one add for one based
             cout<<pos<<"\n";
+            p=lps[p-1];
         }
     }
 }
 int main(){
-    //ios_base::sync_with_stdio(0); cin.tie(0);
     string pat,txt;
     while(cin>>txt>>pat){
         kmp(txt,pat);
